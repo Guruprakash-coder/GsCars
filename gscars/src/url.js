c2cef31c5f80b40441f8b07d665aac06a558eedf
Guruprakash <guruprakash6999@gmail.com>
2025-12-25 15:45:37 +0530

    adding otp feature

--- gscars/src/url.js
+++ gscars/src/url.js
@@ -1,5 +1,5 @@
 // FOR LOCAL TESTING (Uncomment this line when working on laptop)
-// export const baseUrl = "http://localhost:5000";
+//export const baseUrl = "http://localhost:5000";
 
 // FOR PRODUCTION (We will paste the Render link here later)
 export const baseUrl = "https://gscars-web.onrender.com";
